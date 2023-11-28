a4a03e40c73557939168f63a180cf864c9ac4dc1
Guga <gustavo.ferreira0821@gmail.com>
2023-11-28 20:28:33 -0300

    :wrench: configure api base route

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,22 @@
 const functions = require("firebase-functions");
+const cors = require('cors');
+const express = require('express');
+const routes = require('./src/routes/index');
 
-// // Create and deploy your first functions
-// // https://firebase.google.com/docs/functions/get-started
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+const condominiumCashFlowApi = express();
+
+const corsOptions = {
+    'Access-Control-Allow-Origin': ['http://localhost:3000', 'http://localhost:5000', 
+    'http://localhost:5173/'],
+    'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE',
+    'Access-Control-Allow-Headers': 'Content-Type, Authorization',
+    'optionsSuccessStatus': 200,
+}
+
+condominiumCashFlowApi.use(cors(corsOptions));
+
+condominiumCashFlowApi.use(express.json());
+
+condominiumCashFlowApi.use(routes);
+
+exports.condominiumCashFlowApi = functions.https.onRequest(condominiumCashFlowApi);
---
+++ functions/src/routes/index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const router = express.Router();
+
+router.get('/',(req, res)=>{
+    return res.status(200).send({
+        message:"Caixa condomínio API - Welcome"
+    })
+});
+
+module.exports = router;
